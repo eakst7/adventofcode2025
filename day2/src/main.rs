36bb00f982d5fecc97c0afef21c3115a657bd0b0
Ed King <eakst7@yahoo.com>
2025-12-02 22:07:41 -0500

    Day 2 (part2)

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -5,11 +5,11 @@ use std::io::{
 };
 
 fn main() {
-    let r1 = part1();
-    // let r2 = part2();
+    // let r1 = part1();
+    let r2 = part2();
 
-    println!("Part 1: {}", r1.unwrap());
-    // println!("Part 2: {}", r2.unwrap());
+    // println!("Part 1: {}", r1.unwrap());
+    println!("Part 2: {}", r2.unwrap());
 }
 
 fn part1() -> std::io::Result<u64> {
@@ -19,13 +19,26 @@ fn part1() -> std::io::Result<u64> {
 
     let mut matches_sum = 0u64;
     for range in contents.trim().split(",") {
-        matches_sum += process_range(range);
+        matches_sum += process_range_part1(range);
     }
 
     Ok(matches_sum)
 }
 
-fn process_range(range: &str) -> u64 {
+fn part2() -> std::io::Result<u64> {
+    let mut reader = BufReader::new(File::open("input.txt")?);
+    let mut contents = String::new();
+    reader.read_to_string(&mut contents)?;
+
+    let mut matches_sum = 0u64;
+    for range in contents.trim().split(",") {
+        matches_sum += process_range_part2(range);
+    }
+
+    Ok(matches_sum)
+}
+
+fn process_range_part1(range: &str) -> u64 {
     let mut matches = 0u64;
     let (lo, hi) = range.split_once("-").unwrap();
     let lo: u64 = lo.parse().unwrap();
@@ -45,6 +58,54 @@ fn process_range(range: &str) -> u64 {
         if left == right {
             println!("{} {} {}", i, left, right);
             matches += i;
+        }
+    }
+
+    println!("{}", matches);
+    matches
+}
+
+fn process_range_part2(range: &str) -> u64 {
+    let mut matches = 0u64;
+    let (lo, hi) = range.split_once("-").unwrap();
+    let lo: u64 = lo.parse().unwrap();
+    let hi: u64 = hi.parse().unwrap();
+
+    'i: for i in lo..=hi {
+        let istr = i.to_string();
+
+        let mid: usize = ((istr.len() as f32) / 2.0).ceil() as usize;
+
+        'j: for prefixlen in 1..=mid {
+            let prefix = &istr[0..prefixlen];
+
+            let mut start = prefixlen;
+            let mut end = start + prefixlen;
+            
+            if end > istr.len() {
+                // println!("{} {}", i, prefix);
+                continue 'j;
+            }
+
+
+            let mut suffix1 = &istr[start..end];
+            while end < istr.len() && suffix1 == prefix {
+                start = end;
+                end = start + prefixlen;
+
+                if end > istr.len() {
+                    // println!("{} {} {}", i, prefix, suffix1);
+                    continue 'j;
+                }
+                suffix1 = &istr[start..end];
+            }
+            if end == istr.len() && suffix1 == prefix {
+                println!("Match {} {} {}", i, prefix, suffix1);
+                matches += i;
+                continue 'i;
+            }
+
+            // println!("{} {} {}", i, prefix, suffix1);
         }
     }
 
