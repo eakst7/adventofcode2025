b80b3764827679ca33c0bf57775341d5b4b797b8
Ed King <eakst7@yahoo.com>
2025-12-06 16:07:41 -0500

    Day 6 (part 2)

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,15 +1,15 @@
 #![allow(unused_imports)]
 #![allow(unused)]
 use std::fs::{ File, read_to_string };
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Read};
 use aocutils::ulines::UnwrappedLinesExt;
 
 fn main() {
-    let r1 = part1();
-    // let r2 = part2();
+    // let r1 = part1();
+    let r2 = part2();
 
-    println!("Part 1: {}", r1.unwrap());
-    // println!("Part 2: {}", r2.unwrap());
+    // println!("Part 1: {}", r1.unwrap());
+    println!("Part 2: {}", r2.unwrap());
 }
 
 fn part1() -> std::io::Result<u64> {
@@ -54,6 +54,69 @@ fn part1() -> std::io::Result<u64> {
 }
 
 fn part2() -> std::io::Result<u64> {
+    let mut f = File::open("input2.txt").unwrap();
+    let mut b = BufReader::new(f);
+    let mut filedata: Vec<u8> = Vec::new();
 
-    Ok(0)
+    let mut ops: String= String::new();
+    b.read_line(&mut ops).unwrap();
+
+    let ops: Vec<char> = ops.split_whitespace().map(|x| x.chars().nth(0).unwrap()).collect();
+    dbg!(&ops);
+
+    b.read_to_end(&mut filedata);
+
+    let mut column_vecs: Vec<Vec<char>> = Vec::new();
+
+    let mut i = 0;
+    for b in filedata {
+        if b != b'\n' {
+            if column_vecs.len() <= i {
+                column_vecs.push(Vec::new());
+            }
+
+            column_vecs[i].push(b as char);
+            i = i + 1;
+        } else {
+            i = 0;
+        }
+    }
+    
+    dbg!(&column_vecs[0]);
+
+    let mut group = 0;
+    let mut total = 0;
+    let mut group_total = 0;
+    for col in &column_vecs {
+        dbg!(group);
+        let s: String = col.iter().collect::<String>().trim().to_string();
+
+        if s.is_empty() {
+            dbg!(group_total);
+            group += 1;
+            total += group_total;
+            group_total = 0;
+            continue;
+        }
+
+        let op = ops[group];
+        let i: u64 = s.parse().unwrap();
+        // dbg!(i, op);
+
+        if op == '*' {
+            if group_total == 0 {
+                group_total = i;
+            } else {
+                group_total *= i;
+            }
+        } else if op == '+' {
+            group_total += i;
+        } else {
+            panic!();
+        }
+
+    }
+    total += group_total;
+    dbg!(&total);
+    Ok(total)
 }
